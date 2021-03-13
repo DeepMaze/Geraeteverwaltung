8887acb0e617c86667dd296c19777d7ffbafb42e
Adrian Rölleke <adrian.roelleke@bpex.de>
2021-03-13 16:37:28 +0100

    added filter and collapsible items

--- src/app/views/locations/locations.component.ts
+++ src/app/views/locations/locations.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from "../../interfaces/location.interface";
 
 @Component({
   selector: 'app-locations',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LocationsComponent implements OnInit {
 
+  public locations: Array<Location> = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    for (let index = 0; index < 10; index++) {
+      this.locations.push({ name: `Name_${index}`, address: { street: `Straße_${index}`, city: `Stadt_${index}`} })
+    }
+  }
+
+  doFilter(): void {
+
   }
 
 }
