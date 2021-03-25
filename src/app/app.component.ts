5830f1e3f34926875f2e96fef786d8c8c3512768
Adrian Rölleke <adrian.roelleke@gmail.com>
2021-03-25 12:28:49 +0100

    added observable storage service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
+import { AuthenticationService } from "./service/authentication.service";
 
 @Component({
   selector: 'app-root',
@@ -19,7 +20,8 @@ export class AppComponent {
   ]
 
   constructor(
-    private router: Router
+    private router: Router,
+    private authService: AuthenticationService
   ) { }
 
   ngOnInit() {
--- src/app/service/authentication.service.ts
+++ src/app/service/authentication.service.ts
@@ -1,11 +1,29 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
+import { Observable, Subject } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthenticationService {
+export class AuthenticationService implements OnInit {
+
+  public loggedInObservable: Subject<boolean> = new Subject<boolean>();
+  public loggedIn: boolean = false;
 
   constructor() { }
+
+  ngOnInit(): void {
+    this.checkLoggedIn().subscribe((result) => { this.loggedIn = result; });
+  }
+
+  checkLoggedIn(): Observable<boolean> {
+    return new Observable((observer) => {
+      if (localStorage.getItem("token")) {
+        observer.next(true);
+      } else {
+        observer.next(false);
+      }
+    })
+  }
 
   checkToken() {
     return true;
